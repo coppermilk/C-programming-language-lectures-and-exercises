77ee9848bae0f26f9aa106c3a7ac062cb7c6892d
coppermilk <art@coppermilk.org>
2021-06-24 00:45:00 +0300

    Add files via upload

---
+++ ch6ex2.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int pyramids = 8;
+int main ()
+{
+  for (int i = 0; i < pyramids; i++)
+    {
+      for (int j = 0; j <= i; j++)
+	{
+	  printf ("$");
+	}
+      printf ("\n");
+    }
+  return 0;
+}
