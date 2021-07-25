7557a421f414c2ab4af5aea1267df37acb8ba548
coppermilk <art@coppermilk.org>
2021-07-25 23:43:05 +0300

    Create ch11ex08.c

---
+++ ch11/ch11ex08.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+char * string_in(char * str, char * sub);
+int main() {
+        char str[] = "Время всегда хорошее!";
+        char sub[] = "Время";
+        string_in(str, sub);
+
+        return 0;
+}
+
+char * string_in(char * str, char * sub) {
+        if (strstr(str, sub) == NULL) {
+                fprintf(stdout, "'%s' не найдено в строке '%s'", sub, str);
+        } else {
+                fprintf(stdout, "'%s' было найдено в строке '%s' по адрессу %p", sub, str, strstr(str, sub));
+        }
+}
