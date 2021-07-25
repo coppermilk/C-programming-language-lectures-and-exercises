c5613afb6a618915f398d7649b2c8b2a97348178
coppermilk <art@coppermilk.org>
2021-07-25 20:41:46 +0300

    Create ch11ex02.c

---
+++ ch11/ch11ex02.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define SIZE 5
+void fill_array(char ar[SIZE], int size);
+
+int main() {
+        char ar[SIZE];
+        fill_array(ar, SIZE);
+        for (int i = 0; i < SIZE; i++) {
+                putchar(ar[i]);
+        }
+        return 0;
+}
+
+void fill_array(char ar[SIZE], int size) {
+        for (int i = 0; i < size; i++) {
+                ar[i] = getchar();
+                if (ar[i] == '\n' || ar[i] == '\t' || ar[i] == ' ') {
+                        ar[i] = '\0';
+                        break;
+                }
+
+        }
+
+}
